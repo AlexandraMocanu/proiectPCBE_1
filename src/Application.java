d2642aef4b4ced4a0bebc62f6e56a2b5ee292e53
taigi100 <robert.cazacu@oce.com>
2018-11-05 19:05:10 +0200

    Clean up App

--- src/Application.java
+++ src/Application.java
@@ -3,11 +3,11 @@ import java.util.ArrayList;
 public class Application {
     public static void main (String args[]){
 
-        Server server = new Server("Server1");
+        Server server = new Server("Server");
 
         for (int i = 0; i < 5; i++) {
-            LocalClient client = new LocalClient(server);
-            client.registerToServer();
+            LocalClient client = new LocalClient();
+            client.registerToServer(server);
         }
 
     }
